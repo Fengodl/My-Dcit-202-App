bac65761c0c753ef3ee355e5cfd2bd50ab64676c
Avorkliyah Eyram Dennis <87717235+Fengodl@users.noreply.github.com>
2021-11-16 09:04:20 -0800

    Add files via upload

---
+++ Tests/Sagas/StartupSagaTest.js
@@ -0,0 +1,8 @@
+import { select, put } from 'redux-saga/effects'
+import { startup } from '../../App/Sagas/StartupSagas'
+
+const stepper = (fn) => (mock) => fn.next(mock).value
+
+test('watches for the right action', () => {
+  const step = stepper(startup())
+})
